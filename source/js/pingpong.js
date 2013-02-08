5c7902ed428a8bbe6f609e5c270431685e880c68
allomov <lomov.as@gmail.com>
2013-02-08 09:39:05 +0300

    working copy of game

--- source/js/pingpong.js
+++ source/js/pingpong.js
@@ -1,5 +1,28 @@
 $(function(){
     var width = 600;
+    var height = 600;
+    var leftArrowPressed  = false;
+    var rightArrowPressed = false;
+
+
+    var pingpong = new PingpongViewModel(width, height);
+    ko.applyBindings(pingpong);
+    pingpong.start();
+
+    $('body').keydown(function(e){
+        console.log(e.which);
+        if (e.which == 37) { leftArrowPressed  = true }
+        if (e.which == 39) { rightArrowPressed = true }
+    })
+
+    $('body').keyup(function(){
+        rightArrowPressed = leftArrowPressed = false;
+    })
+
+    function v(dx, dy) {  // velocity
+        this.dx = dx;
+        this.dy = dy;
+    }
 
     function Ball(x, y, r, v) {
         var self = this;
@@ -15,36 +38,36 @@ $(function(){
         }
     }
 
-    function v(dx, dy) {
-        this.dx = dx;
-        this.dy = dy;
-    }
-
-    function Player(position, computer) {
-        this.self = this;
+    function Player(options) {
+        var self = this;
+        var position = options['position'];
+        var computer = typeof(options['computer']) != 'undefined' && options['position'];
         this.k = (position == 'up') ? 1 : -1;
-        this.height = 10;
-        this.speed = 10;
-        this.x = ko.observable(width / 2);
-        this.width = 40;
+        this.height = 8;
+        this.width = 120;
+        this.x = ko.observable((width - self.width) / 2);
+        this.y = (position == 'up') ? 0 : height - this.height;
+        this.speed = 3;
         this.moveLeft = function() {
             if (self.x() - self.speed > 0) {
                 self.x(self.x() - self.speed);
             }
         }
         this.moveRight = function() {
-            if (self.x() + self.width + self.speed > width) {
+            if (self.x() + self.width + self.speed < width) {
                 self.x(self.x() + self.speed);
             }
         }
         this.interactWith = function(ball) {
-            if (self.k * self.border > self.k * ball.x()) {
-                ball.v.dx = - ball.v.dx;
+            if (ball.x() > self.x() && ball.x() < self.x() + self.width) {
+                if (self.k * self.y + ball.r + ((position == 'up') ? self.height : 0) > self.k * ball.y()) {
+                    ball.v.dy = - ball.v.dy;
+                }
             }
         }
         if (computer) {
             this.defendFrom = function(ball){
-                if (self.x() > ball.x()) {
+                if (self.x() + self.width / 2  > ball.x()) {
                     self.moveLeft();
                 } else {
                     self.moveRight();
@@ -66,17 +89,38 @@ $(function(){
         }
     }
 
+    function Border(yBorder, position, wonMessage) {
+        var self = this;
+        this.up = (position == 'up') ? true : false;
+        this.k = this.up ? 1 : -1;
+        this.border = yBorder;
+        this.interactWith = function(ball) {
+            if (self.k * self.border + ball.r > self.k * ball.y()) {
+                alert(wonMessage);
+                pingpong.start();
+            }
+        }
+    }
+
     function PingpongViewModel(w, h) {
         var self = this;
-        this.width = ko.observable(w);
-        this.height = ko.observable(h);
-        this.ball   = new Ball(w/2, h/2, 20, new v(-5, 2.5));
-        this.player1   = new Player('#player1');
-        this.player2   = new Player('#player2');
-        this.leftWall  = new Wall(0, 'left');
-        this.rightWall = new Wall(w, 'right');
+        this.width   = w;
+        this.height  = h;
+
+        this.ball    = new Ball(w/2, h/2, 20, new v(5, 2.5));
+        this.player1    = new Player({position: 'up', computer: true});
+        this.player2    = new Player({position: 'down'});
+        this.leftWall   = new Wall(0, 'left');
+        this.rightWall  = new Wall(w, 'right');
+        this.upBorder   = new Border(0, 'up', 'You won! Thanks for good match.');
+        this.downBorder = new Border(h, 'down', 'Computer won! All hail our robots!');
+
+        self.interval = 0;
         this.start = function() {
-            setInterval(self.tick, 30);
+            self.ball.x(self.width/2);
+            self.ball.y(self.height/2);
+            if (self.interval != 0) { clearInterval(self.interval); self.interval = 0; }
+            self.interval = setInterval(self.tick, 30);
         }
         this.tick = function() {
             var ball = self.ball;
@@ -90,10 +134,11 @@ $(function(){
                 }
             }
             self.ball.tick();
+
+            if (leftArrowPressed)  { self.player2.moveLeft();  } else
+            if (rightArrowPressed) { self.player2.moveRight(); }
         }
     }
 
-    var pingpong = new PingpongViewModel(width, 600);
-    ko.applyBindings(pingpong);
-    pingpong.start();
+
 });
